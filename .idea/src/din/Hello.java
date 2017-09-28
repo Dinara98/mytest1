a3875f383fa5cc9d333c60216484fc46c4ec01fe
Karabalaeva Dinara <karabalaeva.d98@mail.ru>
2017-09-29 01:33:19 +0600

    Екинши коммит

--- .idea/src/din/Hello.java
+++ .idea/src/din/Hello.java
@@ -3,6 +3,9 @@ package din;
 public class Hello {
     public static void main(String[]args) {
         System.out.println("Hello World!");
+        System.out.println("Я Динара");
+        System.out.println("И я студент");
+
     }
 
 }
